5030363dafd48bb1d09b1e7c789e39476b619432
hhhscvx <hhhscvx@yandex.ru>
2025-03-26 08:07:43 +0500

    Добавил Docker

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.13-slim
+
+
+WORKDIR /code
+
+COPY ./requirements.txt /code/requirements.txt
+
+RUN pip install -r /code/requirements.txt
+
+COPY ./src /code/app
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
-from src.course.views import router as course_router
+from .course.handlers import router as course_router
 
 
 app = FastAPI()
